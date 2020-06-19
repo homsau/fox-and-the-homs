2d0b27ba3e51633052f783212fc757486d543b64
Rebecca Cunningham <rebeccalynn.awesome@gmail.com>
2020-06-19 19:55:35 -0400

    added react app

---
+++ client/src/components/Countdown/Countdown.js
@@ -0,0 +1,16 @@
+const Countdown = function() {
+    return (
+        <div className="countdown-container">
+            <div className="countdown-holder">
+                <span>{}</span>
+                <span>Days</span>
+                <span>{}</span>
+                <span>Hours</span>
+                <span>{}</span>
+                <span>Minutes</span>
+                <span>{}</span>
+                <span>Seconds</span>
+            </div>
+        </div>
+    )
+}
